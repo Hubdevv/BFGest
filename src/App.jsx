3abe51a2f428a57b78af94b1f73445fe06f0e258
Hania <hania_nina@live.fr>
2023-05-29 07:01:00 +0200

    Dashboard 75%

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
 
 import Login from './pages/Login';
+import { BrowserRouter as Router, Routes, Route   } from 'react-router-dom'; 
+import Dashboard from './pages/Dashboard';
 function App() {
   return (
     <div className="App">
-     <Login/>
+      <Router>
+        <Routes>
+          <Route></Route>
+
+          <Route path="/" element={<Login />}></Route>
+          <Route path='/dashboard' element={<Dashboard/>}></Route>
+
+        </Routes>
+      </Router>
     </div>
   );
 }
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,9 +1,44 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
 function Dashboard() {
   return (
-    <div>
-      
+    <div >
+
+      <header >
+     
+
+        <div className="flex flex-end ai-center">
+            <span className='icon'>l</span>
+            <div>
+                <span className='icon'>m</span>
+            </div>
+            <div className=''>
+                <p>Lyes</p>
+                <p className=''>Super Admin</p>
+            </div>
+           <img src="/image/cat.jpg" alt="" /> 
+         
+       
+        </div>
+      </header>
+
+      <aside>
+        <div className='flex top'>
+            <div className='logo center'>
+                <h1>BF</h1>
+            </div>
+            <p>BFGest</p>
+        </div>
+        <ul>
+            <li><Link to={"/"} className='link' > <span className='icon'>a</span> Tableau de bord</Link> </li>
+            <li><Link to={"/"} className='link' ><span className='icon'>u</span> Clients </Link></li>
+            <li><Link to={"/"} className='link' ><span className='icon'>h</span> Achats</Link></li>
+            <li><Link to={"/"} className='link' ><span className='icon'>s</span> Articles</Link></li>
+            <li><Link to={"/"} className='link' ><span className='icon'>i</span> Fournisseurs</Link></li>
+            <li><Link to={"/"} className='link' ><span className='icon'>o</span> Ventes</Link></li>
+            <li><Link to={"/"}className='link'  ><span className='icon'>y</span> Utilisateurs</Link></li>
+        </ul>
+      </aside>
     </div>
   )
 }
