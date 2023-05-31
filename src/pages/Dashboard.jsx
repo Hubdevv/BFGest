bbc83167d44030d710a5d02fadc0ef11dca46662
Hania <hania_nina@live.fr>
2023-05-31 23:10:05 +0200

    Dashboard 100%

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -2,43 +2,50 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 function Dashboard() {
   return (
-    <div >
+    <div className='col-10' >
 
       <header >
-     
-
-        <div className="flex flex-end ai-center">
-            <span className='icon'>l</span>
-            <div>
-                <span className='icon'>m</span>
+        <div className="flex ai-center">
+            <span className='icon cl2 '>n</span>
+            <div className='pos-r'>
+                <span className='icon cl2'>b</span>
+                <div className="circle"></div>
             </div>
-            <div className=''>
-                <p>Lyes</p>
-                <p className=''>Super Admin</p>
+           <div className='name'>
+                <p className='cl2 '>Lyes</p>
+                <span className='green'>SuperAdmin</span>
             </div>
            <img src="/image/cat.jpg" alt="" /> 
-         
-       
         </div>
       </header>
-
-      <aside>
-        <div className='flex top'>
-            <div className='logo center'>
-                <h1>BF</h1>
-            </div>
-            <p>BFGest</p>
+      <div className='grid2'>
+        <aside>
+                <div className='flex top'>
+                    <div className='logo center'>
+                        <h1>BF</h1>
+                    </div>
+                    <p>BFGest</p>
+                </div>
+                <ul>
+                    <li><Link to={"/"} className='link' > <span className='icon'>a</span> Tableau de bord</Link> </li>
+                    <li><Link to={"/"} className='link' ><span className='icon'>u</span> Clients </Link></li>
+                    <li><Link to={"/"} className='link' ><span className='icon'>h</span> Achats</Link></li>
+                    <li><Link to={"/"} className='link' ><span className='icon'>s</span> Articles</Link></li>
+                    <li><Link to={"/"} className='link' ><span className='icon'>i</span> Fournisseurs</Link></li>
+                    <li><Link to={"/"} className='link' ><span className='icon'>o</span> Ventes</Link></li>
+                    <li><Link to={"/"}className='link'  ><span className='icon'>y</span> Utilisateurs</Link></li>
+                </ul>
+        </aside>
+        <div className="main">
+          <div className="card">
+            <div className="logo"><span className="icon">a</span></div>
+            <h1>Bienvenue <strong>Lyes Djerada</strong></h1>
+            <p>Votre business vous attend</p>
+          </div>
         </div>
-        <ul>
-            <li><Link to={"/"} className='link' > <span className='icon'>a</span> Tableau de bord</Link> </li>
-            <li><Link to={"/"} className='link' ><span className='icon'>u</span> Clients </Link></li>
-            <li><Link to={"/"} className='link' ><span className='icon'>h</span> Achats</Link></li>
-            <li><Link to={"/"} className='link' ><span className='icon'>s</span> Articles</Link></li>
-            <li><Link to={"/"} className='link' ><span className='icon'>i</span> Fournisseurs</Link></li>
-            <li><Link to={"/"} className='link' ><span className='icon'>o</span> Ventes</Link></li>
-            <li><Link to={"/"}className='link'  ><span className='icon'>y</span> Utilisateurs</Link></li>
-        </ul>
-      </aside>
+
+      </div>
+ 
     </div>
   )
 }
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,27 +1,31 @@
 import React, {useState} from 'react'
 import User from '../class/User'
 
-const Connect = async (e) => {
+const Connect = async (e, setError) => {
 
     e.preventDefault()
     let inputs = e.target
     let pass = inputs.pass 
     let email = inputs.email  
 
-    console.log(email);
-
  if( email.value !=='' && pass.value !==''){
 
     console.log("Les champs sont remplis")
+    // console.log(email.value);
+    // console.log('hello ')
 
-    const results = await User.login(inputs);
+    // const results = await User.login(inputs);
 
-    console.log(results)
+    // console.log(results)
+    if(pass.value ==''){setError(true)
+    console.log('hello ')}
 
 }
 }
 
 function Login() {
+
+  const [error, setError] =useState(false)
 
   return (
     <div className='c center'>
@@ -29,8 +33,8 @@ function Login() {
         <h2>Connectez vous</h2>
         <p>Connectez-vous à votre espace administrateur</p>
 
-        <form action='' method='POST' onSubmit={(e) => Connect(e)}>
-
+        <form action='' method='POST' onSubmit={(e) => Connect(e, setError)}>
+            {error? <p className='cl6'>Veuillez saisir votre mot de passe.</p> : ''}
             <label>Email</label>
             <input type="email" name='email' placeholder='Email' required="true" />
 
